d2c1b0013fb993f2b3b3af235c1bc7750324c864
Wu <finne0831@gmail.com>
2020-04-03 18:59:51 +0800

    cleaned up folder

---
+++ fed/main_fed_b10e5.py
@@ -0,0 +1,97 @@
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+from torchvision import datasets, transforms
+import torch
+
+from utils.sampling import mnist_iid, mnist_noniid, cifar_iid
+from utils.options import args_parser
+from models.Update import LocalUpdate
+from models.Nets import MLP, CNNMnist, CNNCifar
+from models.Fed import FedAvg
+from models.test import test_img
+
+
+if __name__ == '__main__':
+    # parse args
+    args = args_parser()
+    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
+    args.gpu = -1
+    args.iid = True
+    args.dataset = 'mnist'
+    args.num_channels = 1
+    args.model = 'cnn'
+
+
+    Epochs = [5] # <- change this
+    B_bs = [10] # <- change this
+
+    acc_dic = {}
+    final_acc = []
+
+    loss_dic = {}
+    final_loss = []
+
+    for i in range(len(Epochs)):
+        args.epochs = Epochs[i]
+        args.local_bs = B_bs[i]
+        
+        # load dataset and split users
+        trans_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        dataset_train = datasets.MNIST('../data/mnist/', train=True, download=True, transform=trans_mnist)
+        dataset_test = datasets.MNIST('../data/mnist/', train=False, download=True, transform=trans_mnist)
+
+        dict_users = mnist_iid(dataset_train, args.num_users)
+        img_size = dataset_train[0][0].shape
+        net_glob = CNNMnist(args=args).to(args.device)
+
+        # copy weights
+        w_glob = net_glob.state_dict()
+
+        # training
+        loss_train = []
+        loss_sample = []
+        cv_loss, cv_acc = [], []
+        val_loss_pre, counter = 0, 0
+        net_best = None
+        best_loss = None
+        val_acc_list, net_list = [], []      
+
+        acc_train = None 
+        loss_sample = None
+        acc_test = None
+        loss_test = None
+
+        for communicate in range(1,501):    
+            # print("i= {}".format(i))
+            # print('Round %d' % communicate)
+
+            for iter in range(args.epochs):
+                w_locals, loss_locals = [], []
+                m = max(int(args.frac * args.num_users), 1)
+                idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+                for idx in idxs_users:
+                    local = LocalUpdate(args=args, dataset=dataset_train, idxs=dict_users[idx])
+                    w, loss = local.train(net=copy.deepcopy(net_glob).to(args.device))
+                    w_locals.append(copy.deepcopy(w))
+                    loss_locals.append(copy.deepcopy(loss))
+                # update global weights
+                w_glob = FedAvg(w_locals)
+
+                # copy weight to net_glob
+                net_glob.load_state_dict(w_glob)
+
+                # print loss
+                loss_avg = sum(loss_locals) / len(loss_locals)
+                #print('Round {:3d}, Average loss {:.3f}'.format(iter, loss_avg))
+                loss_train.append(loss_avg)
+            
+            loss_sample = loss_train
+            net_glob.eval()
+            #acc_train, loss_sample = test_img(net_glob, dataset_train, args)
+            acc_test, loss_test = test_img(net_glob, dataset_test, args)
+            
+            print(acc_test.numpy())
+
--- reproduce/res.m
+++ reproduce/res.m
@@ -3,8 +3,7 @@ clear all;
 
 %% plotting paper fig 2-1 (iid)
 b_idx = [10,50,600];
-e_idx = [1, 5];
-%e_idx = [1, 5, 200];
+e_idx = [1, 5, 20];
 
 figure(87);
 
@@ -40,8 +39,10 @@ end
 xlabel('Communication Rounds');
 ylabel('Test Accuracy (%)');
 title('MNIST CNN IID');
-%legend({'B=10 E=1','B=10 E=5','B=10 E=20','B=50 E=1','B=50 E=5','B=50 E=20','B=\infty E=1','B=\infty E=5','B=\infty E=20'},'Location','southeast');
-%legend('boxoff');
+legend({'B=10 E=1','B=10 E=5','B=10 E=20','B=50 E=1','B=50 E=5','B=50 E=20','B=\infty E=1','B=\infty E=5','B=\infty E=20'},'Location','southeast');
+legend('boxoff');
+axis([0 1000 90 100]);
+
 
 %% plotting paper fig 2-2 (non-iid)
 b_idx = [10,50,600];
@@ -104,9 +105,10 @@ for b = b_idx
     end
     
     for e = e_idx
-        filename = ['mnist_cnn_iid_result\b',num2str(b),'e',num2str(e),'.txt'];
+        filename = ['mnist_cnn_iid_result\ud_b',num2str(b),'e',num2str(e),'.txt'];
         fileID = fopen(filename,'r');
         data = fscanf(fileID,'%f');
+        data = repelem(data,10);
         hold on;
         
         if e == 1
